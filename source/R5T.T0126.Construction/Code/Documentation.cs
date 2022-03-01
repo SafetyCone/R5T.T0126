8fec5bdf118847aa693e21dc2928542cafae535a
davidcoats <david@getsafetycone.com>
2022-03-01 08:49:42 -0800

    Initial commit of source-stub.

---
+++ source/R5T.T0126.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0126.Construction
+{
+    /// <summary>
+    /// Construction project for Typed syntax annotations..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
---
+++ source/R5T.T0126/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0126
+{
+    /// <summary>
+    /// Typed syntax annotations.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
