aa1694618ccae30384d1bd28d28abf8fbdf4d98e
David Coats <david@getsafetycone.com>
2022-03-16 15:29:25 -0700

    Add MethodDeclarationAnnotation class.

---
+++ source/R5T.T0126/Code/Classes/MethodDeclarationAnnotation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+
+namespace R5T.T0126
+{
+    public class MethodDeclarationAnnotation : SyntaxNodeSyntaxAnnotation<MethodDeclarationSyntax>
+    {
+        #region Static
+
+        public static MethodDeclarationAnnotation From(SyntaxAnnotation annotation)
+        {
+            var output = new MethodDeclarationAnnotation(annotation);
+            return output;
+        }
+
+        #endregion
+
+
+        public MethodDeclarationAnnotation(SyntaxAnnotation annotation)
+            : base(annotation)
+        {
+        }
+    }
+}
